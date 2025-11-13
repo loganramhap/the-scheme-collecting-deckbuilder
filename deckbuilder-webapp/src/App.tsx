e1b7e16a911b80d13e3f8fb15d7af6ab018e3e18
Logan Ramhap <44788545+loganramhap@users.noreply.github.com>
2025-11-12 19:41:21 -0500

    Update App.tsx

--- deckbuilder-webapp/src/App.tsx
+++ deckbuilder-webapp/src/App.tsx
@@ -1,5 +1,4 @@
-import { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { useAuthStore } from './store/auth';
